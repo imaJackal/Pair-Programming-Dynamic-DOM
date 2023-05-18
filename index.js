04f0e0018591293cff73727c7cc9659febaa2d64
imaJackal <jhernandez43075@gmail.com>
2023-05-18 15:02:33 -0700

    fixed issues with doc

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const bookmarkList = [];
 
-const inputSection = document.querySelector(`#inputFields`);
-const addButton = document.querySelector(`#addBM`);
-const delButton = document.querySelector(`#delBM`);
-const li = document.querySelector(`bmList`);
+const inputSection = document.getElementById(`#inputFields`);
+
+const addButton = document.querySelector(`.addBm`);
+
+const delButton = document.querySelector(`.delBm`);
+const bookmarkName = document.querySelector(".bookmarkName");
+const linkInput = document.querySelector(".linkInput");
+const ul = document.querySelector(`.bmList`);
 let selectedBookmarkIndex = null;
 
 addButton.addEventListener(`click`, () => {
-  const bookmarkName = inputSection.querySelector("#bookmarkName").value;
-  const linkInput = inputSection.querySelector("#linkInput").value;
-  bookmarkList.push(bookmarkName, linkInput);
-  li.innerHTML = bookmarkList;
+  const book = bookmarkName.value;
+  const link = linkInput.value;
+  const listItem = document.createElement(`li`);
+  if (book === null || link === null){
+    return;
+  }else{
+  listItem.textContent = book
+  bookmarkList.push({book, link});
+  ul.append(listItem);
+  }
   // addBookmark({ name: bookmarkName, url: linkInput });
   // renderAllBookmarks(bookmarkList);
-  console.log(li)
+  console.log(bookmarkList)
 })
 
 // delButton.addEventListener(`click`, () => {
