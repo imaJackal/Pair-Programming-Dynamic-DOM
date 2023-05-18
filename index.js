97af2f09e3a95a34a5aaf634197e263a09504738
imaJackal <jhernandez43075@gmail.com>
2023-05-18 14:14:55 -0700

    added event listeners

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const bookMarkList = [];
+const bookmarkList = [];
 
-const animeVillainSect = document.querySelector(`#inputFields`);
-const linkInput = document.querySelector(`#linkInput`);
-const siteName = document.querySelector(`#bookmarkName`);
+const inputSection = document.querySelector(`#inputFields`);
 const addButton = document.querySelector(`#addBM`);
 const delButton = document.querySelector(`#delBM`);
+const li = document.querySelector(`bmList`);
+let selectedBookmarkIndex = null;
+
+addButton.addEventListener(`click`, () => {
+  const bookmarkName = inputSection.querySelector("#bookmarkName").value;
+  const linkInput = inputSection.querySelector("#linkInput").value;
+  bookmarkList.push(bookmarkName, linkInput);
+  li.innerHTML = bookmarkList;
+  // addBookmark({ name: bookmarkName, url: linkInput });
+  // renderAllBookmarks(bookmarkList);
+  console.log(li)
+})
+
+// delButton.addEventListener(`click`, () => {
+//   if()
+//   selectedBookmarkIndex 
+// })
