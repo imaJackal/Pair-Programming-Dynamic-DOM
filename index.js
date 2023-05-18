133039ba61037ddcf7c155739370307fdcfe45ca
imaJackal <jhernandez43075@gmail.com>
2023-05-18 13:09:13 -0700

    Added index.js/css/html and queryselectors

---
+++ index.js
@@ -0,0 +1,7 @@
+const bookMarkList = [];
+
+const animeVillainSect = document.querySelector(`#inputFields`);
+const linkInput = document.querySelector(`#linkInput`);
+const siteName = document.querySelector(`#bookmarkName`);
+const addButton = document.querySelector(`#addBM`);
+const delButton = document.querySelector(`#delBM`);
